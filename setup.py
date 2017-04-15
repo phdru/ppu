0ff313fda6247ccc2a882d38ed26041fd18b0c5e
Oleg Broytman <phd@phdru.name>
2017-04-15 21:51:41 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 
-from imp import load_source
-from os.path import abspath, dirname, join
-
 try:
     from setuptools import setup
     is_setuptools = True
@@ -10,18 +7,13 @@ except ImportError:
     from distutils.core import setup
     is_setuptools = False
 
-versionpath = join(abspath(dirname(__file__)), '{remove-old-files}', '__version__.py')
-load_source('remove-old-files_version', versionpath)
-# Ignore: E402 module level import not at top of file
-from remove-old-files_version import __version__  # noqa
-
-setup(name='{remove-old-files}',
-      version=__version__,
-      description='Broytman {remove-old-files}',
+setup(name='remove-old-files',
+      version='0.0.1',
+      description='Broytman remove-old-files',
       long_description=open('README.txt', 'rtU').read(),
       author='Oleg Broytman',
       author_email='phd@phdru.name',
-      url='http://phdru.name/Software/Python/',
+      url='http://phdru.name/Software/Python/remove-old-files/',
       license='GPL',
       platforms=['any'],
       keywords=[''],
@@ -37,8 +29,6 @@ setup(name='{remove-old-files}',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 2 :: Only',
       ],
-      packages=['{remove-old-files}'],
-      package_data={'{remove-old-files}': []},
       scripts=[],
       requires=[],
       )
