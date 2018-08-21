43f29326e4070f7c243ef89922c25b0d4a44c257
Oleg Broytman <phd@phdru.name>
2018-08-21 19:54:46 +0300

    Drop support for Python 3.3

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
@@ -48,5 +47,5 @@ setup(
         'scripts/cmp.py', 'scripts/remove-old-files.py', 'scripts/rm.py',
         'scripts/which.py',
     ],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
 )
